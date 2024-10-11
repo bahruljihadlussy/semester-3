976ed0fc0fa3649c3a8ffdfc9f55c8e26d35b323
bahruljihadlussy <maadlussy12@gmail.com>
2024-10-11 10:01:01 +0700

    new file:   MyClass.java 	modified:   web1.html 	modified:   web2.html 	new file:   web3.html 	new file:   web4.html

---
+++ MyClass.java
@@ -0,0 +1,12 @@
+public class MyClass {
+  public static void main (String [] args) {
+    String FristName = "Bahrul";
+    String lastName = "Lussy";
+    String fullName = FristName + lastName;
+    System.out.println((fullName));
+
+
+ 
+  }
+}
+  
